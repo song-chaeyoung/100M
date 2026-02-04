ff63b6ac31533fe2670bcaad745efb9df8420cd3
SongChaeyoung <call0512@naver.com>
2026-02-04 09:08:02 +0900

    fix: 로그인 설정 원복

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,18 @@
 import type { NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
+import Kakao from "next-auth/providers/kakao";
 
 export const authConfig = {
-  providers: [],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Kakao({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+  ],
   pages: {
     signIn: "/login",
   },
