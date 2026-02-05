dd5625494ff3a367690af2391b7aa1333a7ac43b
SongChaeyoung <call0512@naver.com>
2026-02-05 15:42:47 +0900

    feat: meatadata viewport 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import { ThemeProvider } from "@/components/providers/theme-provider";
@@ -18,16 +18,17 @@ const pretendard = localFont({
   ],
 });
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: "cover",
+  interactiveWidget: "resizes-content",
+};
+
 export const metadata: Metadata = {
   title: "일억모으기",
   description: "가계부 앱",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    viewportFit: "cover",
-    interactiveWidget: "resizes-content",
-  },
 };
 
 export default function RootLayout({
